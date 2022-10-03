e531219fe6599af4abe1c002a2b9ab1c86fde8d8
geronimobknuttzen <geronimo.bknuttzen@gmail.com>
2022-10-02 23:30:07 -0300

    corrigiendo Estructura

--- api/index.js
+++ api/index.js
@@ -19,7 +19,6 @@ app.use(logger('combined'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 //Routes
 
