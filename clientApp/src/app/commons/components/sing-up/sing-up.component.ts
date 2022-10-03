a89a65a7d12727a36dc5291f97e105dca46a833b
Alexander <alexanderjfigueredo@gmail.com>
2022-10-03 15:49:43 -0500

    Primer avance del registro

--- clientApp/src/app/commons/components/sing-up/sing-up.component.ts
+++ clientApp/src/app/commons/components/sing-up/sing-up.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-sing-up',
@@ -10,6 +12,17 @@ export class SingUpComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+  hide = true;
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
   }
 
 }
