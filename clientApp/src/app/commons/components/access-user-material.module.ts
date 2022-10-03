26d2118dbe4b8af58807067158475d7c602fc902
Alexander <alexanderjfigueredo@gmail.com>
2022-10-03 15:30:05 -0500

    Primer avance de login

---
+++ clientApp/src/app/commons/components/access-user-material.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoginComponent } from './login/login.component';
+import { SingUpComponent } from './sing-up/sing-up.component';
+import { ResetPassComponent } from './reset-pass/reset-pass.component';
+import { ContactoComponent } from './contacto/contacto.component';
+
+
+// componentes materia
+import { FormSharedModule } from '../shared/form-shared.module';
+
+
+@NgModule({
+  declarations: [
+    LoginComponent, SingUpComponent, ResetPassComponent, ContactoComponent
+  ],
+  imports: [
+    CommonModule,FormSharedModule,
+  ],
+  exports:[
+    
+    LoginComponent, SingUpComponent, ResetPassComponent, ContactoComponent,
+
+    // componentes de material
+  ]
+
+})
+export class AccessUserMaterialModule { }
