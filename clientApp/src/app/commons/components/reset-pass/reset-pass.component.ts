89ce1d24837753e0f9b1c174ab029913c3e59475
Alexander <alexanderjfigueredo@gmail.com>
2022-10-03 15:57:29 -0500

    Primer avance del Reset pass

--- clientApp/src/app/commons/components/reset-pass/reset-pass.component.ts
+++ clientApp/src/app/commons/components/reset-pass/reset-pass.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-reset-pass',
@@ -12,4 +13,15 @@ export class ResetPassComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  email = new FormControl('', [Validators.required, Validators.email]);
+  hide = true;
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+  
 }
