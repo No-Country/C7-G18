ca2b3f5aff87f331ac11f90f0c77686ffcb03f3e
Emanuel-Capo <emanuel_c400@hotmail.com>
2022-10-06 18:06:00 -0300

    home page mejorada

--- clientApp/src/app/commons/layouts/store-layout/components/navbar/navbar.component.ts
+++ clientApp/src/app/commons/layouts/store-layout/components/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { LoginComponent } from 'src/app/commons/components/login/login.component';
 
+
 @Component({
 	selector: 'app-navbar',
 	templateUrl: './navbar.component.html',
@@ -9,6 +10,12 @@ import { LoginComponent } from 'src/app/commons/components/login/login.component
 })
 export class NavbarComponent implements OnInit {
 	constructor(private _matDialog: MatDialog) {}
+
+	panelOpenState: boolean = false;
+
+	togglePanel() {
+    	this.panelOpenState = !this.panelOpenState
+	}
 
 	openModalLogin() {
 		if (screen.width < 500) {
