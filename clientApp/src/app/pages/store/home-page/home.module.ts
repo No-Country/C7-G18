562d28372f639a50df038183b52ee45c42f00303
Emanuel-Capo <emanuel_c400@hotmail.com>
2022-10-10 01:02:57 -0300

    store en proceso

--- clientApp/src/app/pages/store/home-page/home.module.ts
+++ clientApp/src/app/pages/store/home-page/home.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormSharedModule } from '../../../commons/shared/form-shared.module';
 import { HomePageComponent } from './home-page.component';
-import { CardProductComponent } from '../../../commons/components/card-product/card-product.component';
 import { BannerComponent } from './component/banner/banner.component';
 import { CategoriasComponent } from './component/categorias/categorias.component';
 import { BannerPrincipalComponent } from './component/banner-principal/banner-principal.component';
 import { MarcasComponent } from './component/marcas/marcas.component';
+import { CardProductModule } from 'src/app/commons/components/card-product/card-product.module';
 
 export const routes: Routes = [{ path: '', component: HomePageComponent }];
 //el FormSharedModule ya tiene los buttons, el check, inputs, y bueno lo necesario, por lo que 
 //ya no es necesario importar uno por uno en este modulo
 @NgModule({
-    declarations: [HomePageComponent, CardProductComponent, BannerComponent, CategoriasComponent, BannerPrincipalComponent, MarcasComponent],
-    imports: [RouterModule.forChild(routes), FormSharedModule]
+    declarations: [HomePageComponent, BannerComponent, CategoriasComponent, BannerPrincipalComponent, MarcasComponent],
+    imports: [RouterModule.forChild(routes), FormSharedModule, CardProductModule]
 })
 export class HomeModule { }
 
--- clientApp/src/app/pages/store/store-page/store-page.component.ts
+++ clientApp/src/app/pages/store/store-page/store-page.component.ts
@@ -1,4 +1,134 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Observable } from 'rxjs';
+import { CardProduct } from 'src/app/commons/components/card-product';
+
+
+const cards:CardProduct[]=[
+  {
+    img:'assets/images/image 17.svg',
+    nombre:'MIMASKOT Gato Adulto - Pollo y Carne - 9Kg',
+    precio:19.99,
+    meGusta:false,
+    vendidos:35
+  },
+  {
+    img:'assets/images/image 65.svg',
+    nombre:'MIMASKOT Gato Adulto - Pollo y Carne - 9Kg',
+    precio:15.99,
+    meGusta:true,
+    vendidos:5
+  },
+  {
+    img:'assets/images/image 17.svg',
+    nombre:'MIMASKOT Gato Adulto - Pollo y Carne - 9Kg',
+    precio:9.99,
+    meGusta:false,
+    oferta:5.99,
+    nuevo:true,
+    vendidos:40
+  },
+  {
+    img:'assets/images/image 17.svg',
+    nombre:'MIMASKOT Gato Adulto - Pollo y Carne - 9Kg',
+    precio:19.99,
+    meGusta:false,
+    nuevo:true,
+    vendidos:10
+  },
+  {
+    img:'assets/images/image 65.svg',
+    nombre:'MIMASKOT Gato Adulto - Pollo y Carne - 9Kg',
+    precio:15.99,
+    meGusta:false,
+    oferta:12.99,
+    nuevo:true,
+    vendidos:1
+  },
+  {
+    img:'assets/images/image 17.svg',
+    nombre:'MIMASKOT Gato Adulto - Pollo y Carne - 9Kg',
+    precio:9.99,
+    meGusta:false,
+    vendidos:15
+  },
+  {
+    img:'assets/images/image 17.svg',
+    nombre:'MIMASKOT Gato Adulto - Pollo y Carne - 9Kg',
+    precio:9.99,
+    meGusta:false,
+    oferta:5.99,
+    nuevo:true,
+    vendidos:48
+  },
+  {
+    img:'assets/images/image 17.svg',
+    nombre:'MIMASKOT Gato Adulto - Pollo y Carne - 9Kg',
+    precio:19.99,
+    meGusta:false,
+    vendidos:4
+  },
+  {
+    img:'assets/images/image 65.svg',
+    nombre:'MIMASKOT Gato Adulto - Pollo y Carne - 9Kg',
+    precio:15.99,
+    meGusta:false,
+    oferta:12.99,
+    nuevo:true,
+    vendidos:100
+  },
+  {
+    img:'assets/images/image 65.svg',
+    nombre:'MIMASKOT Gato Adulto - Pollo y Carne - 9Kg',
+    precio:9.99,
+    meGusta:false,
+    vendidos:37
+  },
+  {
+    img:'assets/images/image 65.svg',
+    nombre:'MIMASKOT Gato Adulto - Pollo y Carne - 9Kg',
+    precio:15.99,
+    meGusta:false,
+    oferta:12.99,
+    nuevo:true,
+    vendidos:100
+  },
+  {
+    img:'assets/images/image 17.svg',
+    nombre:'MIMASKOT Gato Adulto - Pollo y Carne - 9Kg',
+    precio:9.99,
+    meGusta:false,
+    vendidos:37
+  },
+  {
+    img:'assets/images/image 65.svg',
+    nombre:'MIMASKOT Gato Adulto - Pollo y Carne - 9Kg',
+    precio:15.99,
+    meGusta:false,
+    oferta:12.99,
+    nuevo:true,
+    vendidos:100
+  },
+  {
+    img:'assets/images/image 65.svg',
+    nombre:'MIMASKOT Gato Adulto - Pollo y Carne - 9Kg',
+    precio:9.99,
+    meGusta:false,
+    vendidos:37
+  },
+  {
+    img:'assets/images/image 65.svg',
+    nombre:'MIMASKOT Gato Adulto - Pollo y Carne - 9Kg',
+    precio:15.99,
+    meGusta:false,
+    oferta:12.99,
+    nuevo:true,
+    vendidos:100
+  },
+]
+
+
 
 @Component({
   selector: 'app-store-page',
@@ -7,9 +137,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StorePageComponent implements OnInit {
 
+  cards:CardProduct[]=cards
+
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
+  pet:string='Conejos'
+  category:string='Alimentación'
+  subcategory:string|null=null
+
+
+  pets:string[]=['Gatos','Peros', 'Aves', 'Peces','Conejos']
+  categories:string[]=['Todos', 'Alimentación', 'Accesorios', 'Higiene', 'Farmacia', 'Caniles', 'Juguetes', 'Ropa']
+  subcategories:string[]=[]
+  marcas:string[]=['Todos','Brefar','Brit Care','Dog Chow','Mirrapel','Pro Plan','Vet Life']
+
+
+  //slider
+  max = 1000;
+  min = 0;
+  step = 10;
+  thumbLabel = true;
+  value = this.max;
+  tickInterval = 1;
+
+
+ //paginador cards
+  dataSource:MatTableDataSource<CardProduct> = new MatTableDataSource<CardProduct>(this.cards);
+  obs: Observable<any>;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;    
+    this.obs = this.dataSource.connect();
+  }
+  
+
+
+
+
+
+
+  
 }
