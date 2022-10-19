60db9a5c0402bd7dd064fe38c2e53c4ca62f7818
yovana888 <yovana.otaku@gmail.com>
2022-10-18 20:41:22 -0500

    slider

--- clientApp/src/app/pages/store/home-page/component/marcas/marcas.component.ts
+++ clientApp/src/app/pages/store/home-page/component/marcas/marcas.component.ts
@@ -1,38 +1,41 @@
 import { Component, OnInit , ElementRef, ViewChild} from '@angular/core';
 import KeenSlider, { KeenSliderInstance } from "keen-slider"
+import { Observable, of } from 'rxjs';
 import { BrandService } from 'src/app/commons/services/brand.service';
 import { IBrand } from '../../../../../commons/interfaces/front.interface';
 
 @Component({
   selector: 'app-marcas',
   templateUrl: './marcas.component.html',
-  styleUrls: ['../../../../../../../node_modules/keen-slider/keen-slider.min.css',
+  styleUrls: [
   './marcas.component.scss']
 })
 export class MarcasComponent implements OnInit {
 
-  brands:IBrand[]=[]
-
+  brands: Observable<IBrand[]>
+  @ViewChild("sliderRef") sliderRef!: ElementRef<HTMLElement>
+  
+  slider!: KeenSliderInstance 
+  doResize: boolean = false
   constructor(private brandService:BrandService) {
-     this.brandService.getBrand().subscribe({
-    next: brands=>this.brands=brands,
-    complete:()=>{console.log(this.brands)}
-   })
+    
    }
 
   ngOnInit(): void {
-    
+    this.brandService.getBrand().subscribe({
+      next: (brands)=>{this.brands=of(brands), console.log('next', brands); this.silder()}
+    })
   }
-
-
   
-  @ViewChild("sliderRef") sliderRef!: ElementRef<HTMLElement>
 
-  slider!: KeenSliderInstance
-
-  ngAfterViewInit() {
+  silder() {
     console.log('slider iniciado')
+    const animation = { duration: 25000, easing: (t: any) => t }
     this.slider = new KeenSlider(this.sliderRef.nativeElement, {
+      loop: true,
+      renderMode: "performance",
+      drag: true,
+      initial: 6,
       breakpoints: {
         "(min-width: 400px)": {
           slides: { perView: 2, spacing: 5 },
@@ -47,40 +50,17 @@ export class MarcasComponent implements OnInit {
           slides: { perView: 5, spacing: 10 },
         },
       },
-      slides: { perView: 1 },
-    
-      loop: true,
-    },
-    [
-      (slider) => {
-        let timeout:any
-        let mouseOver = false
-        function clearNextTimeout() {
-          clearTimeout(timeout)
-        }
-        function nextTimeout() {
-          clearTimeout(timeout)
-          if (mouseOver) return
-          timeout = setTimeout(() => {
-            slider.next()
-          }, 2000)
-        }
-        slider.on("created", () => {
-          slider.container.addEventListener("mouseover", () => {
-            mouseOver = true
-            clearNextTimeout()
-          })
-          slider.container.addEventListener("mouseout", () => {
-            mouseOver = false
-            nextTimeout()
-          })
-          nextTimeout()
-        })
-        slider.on("dragStarted", clearNextTimeout)
-        slider.on("animationEnded", nextTimeout)
-        slider.on("updated", nextTimeout)
+      created(s) {
+        s.moveToIdx(5, true, animation)
       },
-    ])
+      updated(s) {
+        s.moveToIdx(s.track.details.abs + 5, true, animation)
+      },
+      animationEnded(s) {
+        s.moveToIdx(s.track.details.abs + 5, true, animation)
+      },
+
+    })
   }
 
   ngOnDestroy() {
