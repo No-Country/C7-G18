fd931a358d1d41e2e4ebb5a2d383392ec879fdea
yovana888 <yovana.otaku@gmail.com>
2022-10-26 15:18:01 -0500

    preformace images

--- clientApp/src/app/commons/layouts/store-layout/store-layout.module.ts
+++ clientApp/src/app/commons/layouts/store-layout/store-layout.module.ts
@@ -4,15 +4,16 @@ import { StoreLayoutComponent } from './store-layout.component';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component'; //para usar el router oulet
-import {MatButtonModule} from '@angular/material/button';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatExpansionModule} from '@angular/material/expansion';
+import { MatButtonModule} from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { SingUpModule } from '../../components/sing-up/sing-up.module';
 import { LoginModule } from '../../components/login/login.module';
 import { ResetPassModule } from '../../components/reset-pass/reset-pass.module';
 import { ContactoModule } from '../../components/contacto/contacto.module'; 
 import { ShoppingCartModule } from '../../components/shopping-cart/shopping-cart.module';
 import { AccountModule } from '../../components/account/account.module';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -28,6 +29,7 @@ import { AccountModule } from '../../components/account/account.module';
          ResetPassModule, 
          ContactoModule,
          ShoppingCartModule,
+         MatIconModule,
         AccountModule],
     exports: [StoreLayoutComponent],
 })
